8cfcc409e8369dc289362e469bc6934d090eadb7
Chris Walther Andersen <cvvand@gmail.com>
2023-11-16 22:05:48 +0100

    Added a startup case to main

--- src/shrew/__main__.py
+++ src/shrew/__main__.py
@@ -1,8 +1,32 @@
-import shrew
-
-
 def main():
-    print("1 + 2 =", shrew.add(1, 2), ".")
+    print("Starting SHREW")
+    print()
+    print()
+
+    print("Experiment setup:")
+    print("Factors(Levels): Age(<40/Y, 41-70/G, >70/O), sex(M,F), progression(1,2,3)")
+    print()
+
+    print("Treatments: Control / Intervention")
+    print()
+
+    print("Current enrollment status:")
+    print("Total: 51 subjects")
+    print("YM1: 1/0 ; YM2: 0/0 ; YM3: 0/0")
+    print("YF1: 2/1 ; YF2: 2/1 ; YF3: 0/1")
+    print("GM1: 1/3 ; GM2: 0/1 ; GM3: 0/2")
+    print("GF1: 4/2 ; GF2: 3/3 ; GF3: 2/0")
+    print("OM1: 5/2 ; OM2: 3/1 ; OM3: 0/3")
+    print("OF1: 1/1 ; OF2: 1/2 ; OF3: 0/3")
+    print()
+
+    print("Randomisation procedure: ")
+    print(" - Deviation measure:   range")
+    print(" - Factor weights:      unit")
+    print(" - Probability:         0.8/0.2")
+    print()
+
+    print("New subject: GM1")
 
 
 if __name__ == "__main__":
