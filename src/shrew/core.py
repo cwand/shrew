12b43c12a6787eb631d7198b8536703c60c0a4fe
Chris Walther Andersen <cvvand@gmail.com>
2023-11-18 20:08:37 +0100

    Working on enrollment files

--- src/shrew/core.py
+++ src/shrew/core.py
@@ -1,2 +1,18 @@
-def add(x: int, y: int) -> int:
-    return x + y
+import numpy as np
+import numpy.typing as npt
+
+
+def read_enroll_file(filename: str, levels_per_factor: list[int],
+                     n_treatments: int) -> npt.ArrayLike:
+    mat_shape = levels_per_factor.copy()
+    mat_shape.append(n_treatments)
+    mat_shape2 = tuple(mat_shape)
+    enrolls = np.zeros(mat_shape2)
+
+    with (open(filename) as f):
+        f.readline()  # HEADER DATA
+        for line in f:
+            enrolls[tuple(map(
+                int, line.split(',')))] = enrolls[tuple(map(int, line.split(',')))] + 1
+
+    return enrolls
--- test/test_core.py
+++ test/test_core.py
@@ -1,9 +1,18 @@
 import unittest
+import numpy as np
 import shrew
+import os
 
 
-class TestTester(unittest.TestCase):
+class TestReadEnrollFile(unittest.TestCase):
 
-	def test_add(self):
-		t = shrew.add(2, 3)
-		self.assertEqual(t, 5)
+	def test_read_empty_file(self):
+		c = shrew.read_enroll_file(os.path.join('test', 'enroll1.txt'), [3, 2, 3], 2)
+		np.testing.assert_array_equal(c, np.zeros((3, 2, 3, 2)))
+
+	def test_read_enroll_2x2x2_file(self):
+		c = shrew.read_enroll_file(os.path.join('test', 'enroll2.txt'), [2, 2], 2)
+		exp_mat = np.zeros((2, 2, 2))
+		exp_mat[1, 0, 0] = 1
+		exp_mat[0, 0, 1] = 2
+		np.testing.assert_array_equal(c, exp_mat)
